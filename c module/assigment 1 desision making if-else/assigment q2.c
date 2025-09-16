a248d6bce334aee1aba3a061b06fb6fc85e9db67
Ramkrushna05 <bargalramkrushna9@gmail.com>
2025-09-16 19:44:52 +0530

    Add files via upload

---
+++ c module/assigment 1 desision making if-else/assigment q2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int palandrom();
+void main ()
+{
+ palandrom();	
+}
+int palandrom()
+{
+	
+	int num ;
+	printf("enter three digit number ");
+	scanf("%d",&num);
+	int r1,r2,r3,q1,rev;
+	r1=num%10;
+	q1=num/10;
+	r2=q1%10;
+	r3=q1/10;
+	rev=r1*100+r2*10+r3;
+	if(num==2)
+	printf("this is palandrom number");
+	else 
+	printf("this is not palandrom number");
+	
+	
+}
