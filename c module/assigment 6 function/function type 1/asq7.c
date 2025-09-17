d772c949b642464668645887a1ca1524b1538318
Ramkrushna05 <bargalramkrushna9@gmail.com>
2025-09-17 22:48:05 +0530

    Add files via upload

---
+++ c module/assigment 6 function/function type 1/asq7.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void salary();
+void main ()
+{
+	salary();
+}
+void salary()
+{
+
+	float bs =7000;
+	float da ,ta ,ts,hra,total;
+	if(bs<=5000)
+	{
+		da=bs*0.10;
+		ta=bs*0.10;
+		hra=bs*0.20;
+	}
+	else
+	{
+	da=bs*0.15;
+	ta=bs*0.25;
+	hra=bs*0.10;	
+	}
+	printf("da=%2f\n",da);
+	printf("ta=%2f\n",ta);
+	printf("hra=%2f\n",hra);
+	printf("total salary=%2f\n",total);
+}
